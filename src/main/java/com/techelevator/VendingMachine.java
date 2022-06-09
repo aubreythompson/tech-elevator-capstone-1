592c5ec6e51924a20fe1b9504a3c4dab60416d36
John Chmiel <johnnytdawg@gmail.com>
2022-06-09 12:38:32 -0400

    makeChange and feedmoney functionality

---
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -0,0 +1,50 @@
+package com.techelevator;
+
+public class VendingMachine {
+    private double currentBalance = 0;
+
+    public Double getCurrentBalance(){
+        return currentBalance;
+    }
+
+    public double feedMoney(Object amount){
+        if (amount == null){
+            System.out.println("Invalid dollar amount entered, please try again");
+            return -1;
+        }
+        try{
+            int moneyFed = Integer.parseInt(amount.toString().replace("$", ""));
+            this.currentBalance += moneyFed;
+            return currentBalance;
+        } catch (NumberFormatException e){
+
+        }
+        System.out.println("invalid dollar amount entered, please try again");
+        return -1;
+    }
+
+    public int[] makeChange(){
+        double[] changeValue = {.25, .10, .05, .01};
+        int quarters = 0;
+        int dimes = 0;
+        int nickels = 0;
+        int pennies = 0;
+        while (currentBalance > 0){
+            if (currentBalance >= changeValue[0]){
+                currentBalance -= changeValue[0];
+                quarters++;
+            } else if (currentBalance >= changeValue[1]){
+                currentBalance -= changeValue[1];
+                dimes++;
+            }else if (currentBalance >= changeValue[2]){
+                currentBalance -= changeValue[2];
+                dimes++;
+            }else{
+                currentBalance -= changeValue[3];
+                pennies++;
+            }
+        }
+        int[] change = {quarters, dimes, nickels, pennies};
+        return change;
+    }
+}
